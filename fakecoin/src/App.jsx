fbde692ae7511d812b8fea79d66fd3373cd74734
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-30 17:25:40 +0500

    bs

--- fakecoin/src/App.jsx
+++ fakecoin/src/App.jsx
@@ -1,6 +1,6 @@
 import  { useState } from 'react';
 import './App.css';
-         
+          
 function App() {
   const [numCoins, setNumCoins] = useState('');
   const [fakeCoinIndex, setFakeCoinIndex] = useState(null);
