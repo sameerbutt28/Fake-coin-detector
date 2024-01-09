8bcbe9a2ff3c6404e4a788ea7d0edb3184ee3d48
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-09 14:48:40 +0500

    refactoring

--- fakecoin/src/App.jsx
+++ fakecoin/src/App.jsx
@@ -1,7 +1,5 @@
 import  { useState } from 'react';
 import './App.css';
-import './App.css'; 
-              
 function App() {
   const [numCoins, setNumCoins] = useState('');
   const [fakeCoinIndex, setFakeCoinIndex] = useState(null);
