1467578e0faf4db834bc97af5d682df830351528
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-18 00:08:07 +0500

    BS

--- fakecoin/src/App.jsx
+++ fakecoin/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   const [numCoins, setNumCoins] = useState('');
   const [fakeCoinIndex, setFakeCoinIndex] = useState(null);
-                                     
+                                        
   const randomizeCoins = (n) => {
     const realCoin = 1;
     const fakeCoin = 0;
